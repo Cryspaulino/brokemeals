f99e5dc48c9097b5fa05be7f60e7e5af1bf40388
Cryspaulino <pau22008@byui.edu>
2025-06-02 15:00:25 -0600

    importing the other css

--- brokemeals-react/src/index.js
+++ brokemeals-react/src/index.js
@@ -2,6 +2,7 @@
 import React from 'react'; // Reac's core library
 import ReactDOM from 'react-dom/client'; // Enables our "root" calls for rendering in the browser.
 import './styles/styles.css'; // Global styles.css import
+import './styles/styles-recipes.css';
 import App from './App'; // Imports the main App component.
 import 'bootstrap/dist/css/bootstrap.min.css';
 
