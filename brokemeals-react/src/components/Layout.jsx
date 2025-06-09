cf7708753c0f47c22f2df1094fb9d94c98ffe3b3
Joyce Ili <131714669+JoyceJ53@users.noreply.github.com>
2025-06-09 14:50:19 -0600

    wireframe changes

--- brokemeals-react/src/components/Layout.jsx
+++ brokemeals-react/src/components/Layout.jsx
@@ -13,9 +13,9 @@ function Layout() {
           <a href="/">
             <img className="logo" src="/images/logo.jpg" alt="Broke Meals Logo" />
           </a>
-          <h1 className="title">Welcome to Broke Meals!</h1>
+          <h1 className="title">Broke Meals</h1>
           <Link className="nav_home" to="/">Home</Link>
-          <Link className="nav_goals" to="/goals">Goals</Link>
+          <Link className="nav_goals" to="/goals">Recipes</Link>
           <Link className="nav_recipes" to="/recipes">Saved Recipes</Link>
           <Link className="sign_in" to="/Signin">Sign In</Link>
         </nav>
@@ -26,7 +26,24 @@ function Layout() {
       </main>
 
       <footer>
-        <p>Beus, Ili, Kafle, Nelson, Paulino Cabrera, Poole &copy; 2025 Broke Meals</p>
+        <div className="footer-logo-and-social-media-box">
+          <img className="footer-logo" src="/images/logo.jpg" alt="Broke Meals Logo" />
+        </div>
+        <div className= "footer-address-and-copyright-box">
+          <h3 className="address">Address: 123 Main St, Anytown, USA</h3>
+        </div>
+        <div className= "footer-subscribe-box">
+          <h2 className= "footer-subscribe-title">Subscribe</h2>
+          <h3 className="footer-subscribe-text">Enter your email to subscibe to our newsletter below:</h3>
+          <input className="footer-subscribe-input" type="email" placeholder="Enter your email" />
+          <button className="footer-subscribe-button">Subscribe</button>
+          <p className= "footer-subscribe-agreement">By subscribing, you agree to recieve emails from us and acknowledge our Privacy Policy</p>
+        </div>
+        <div className= "footer-copyright-box">
+          <p className= "footer-copyright">Beus, Ili, Kafle, Nelson, Paulino Cabrera, Poole &copy; 2025 Broke Meals</p>
+          <p className= "footer-privacy-policy"></p>
+          <p className= "footer-terms-of-use"></p>
+        </div>
       </footer>
     </>
   );
--- brokemeals-react/src/pages/Home.jsx
+++ brokemeals-react/src/pages/Home.jsx
@@ -7,17 +7,27 @@ import '../styles/styles.css';
 function Home() {
   return (
     <>
-      <div className= "paragraph-box">
-        <h3 className= "intro-paragraph">
+      <div className= "home-heading-box">
+        <h1 className= "home-heading">Short Heading goes here</h1>
+        <h3 className= "heading-paragraph">
           We are a group of college students that want to help you get through college while eating healthy and saving money!
           Click the button below to get started!
         </h3>
 
         {/* our button is wrapped in a link to the Recipes.jsx page */}
       
-        <Link to="/recipes" className="get-started-link">
+        <Link to="/tutorial" className="get-started-link">
           <button className="get-started">Get Started</button>
         </Link>
+        <div className= "home-heading-links">
+
+        </div>
+      </div>
+      <div className= "featured-recipe-box">
+        <h1 className= "featured-recipe-title">Featured Recipe: </h1>
+        {/* INSERT FEATURE RECIPE CODE HERE */}
+        {/* INSERT FEATURE RECIPE CODE HERE */}
+        {/* INSERT FEATURE RECIPE CODE HERE */}
       </div>
     </>
   );
