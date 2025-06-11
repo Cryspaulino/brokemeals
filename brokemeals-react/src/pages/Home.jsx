474c74363562639f9541ec3dca3b60a47e4c422a
Joyce Ili <131714669+JoyceJ53@users.noreply.github.com>
2025-06-11 14:29:31 -0600

    home style sheet layout

--- brokemeals-react/src/pages/Home.jsx
+++ brokemeals-react/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 // This is the home page.
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/styles.css';
+import '../styles/styles-home.css';
 
 
 function Home() {
   return (
     <>
       <div className= "home-heading-box">
-        <h1 className= "home-heading">Short Heading goes here</h1>
+        <h1 className= "home-heading">Welcome to broke meals!</h1>
         <h3 className= "heading-paragraph">
           We are a group of college students that want to help you get through college while eating healthy and saving money!
           Click the button below to get started!
@@ -16,8 +16,8 @@ function Home() {
         
         {/* our button is wrapped in a link to the Recipes.jsx page */}
       
-        <Link to="/tutorial" className="get-started-link">
-          <button className="get-started">Get Started</button>
+        <Link to="/tutorial" className="home-get-started-link">
+          <button className="home-get-started-button">Get Started</button>
         </Link>
         <div className= "home-heading-links">
           <Link className="home" to="/">Home</Link>
