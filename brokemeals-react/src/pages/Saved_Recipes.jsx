16ac1b45c9fcf4a1e7e0d42bbce48aadf73f52e5
ajbeus <168456943+ajbeus@users.noreply.github.com>
2025-06-16 14:31:30 -0600

    moved login message to saved recipes

--- brokemeals-react/src/pages/Saved_Recipes.jsx
+++ brokemeals-react/src/pages/Saved_Recipes.jsx
@@ -1,13 +1,25 @@
 // This component represents the "Saved Recipes" page. It will eventually collect the user's saved recipes in a form (I'm assuming).
-import React from 'react';
 import '../styles/styles.css';
+import React, { useEffect, useState } from "react";
 
 function Saved_Recipes() {
-  return (
+    const [user, setUser] = useState(null);
+    // Page if user is not logged in
+    if (!user) {
+    return (
+      <div className= "not-logged-in">
+        <h1 className= "not-logged-in-message">🍲 Please log in to view your saved recipes. 🍲</h1>
+      </div>
+    );
+  }
+  else{
+      return (
     <div>
       {/* You can add form inputs or interactive elements here */}
     </div>
   );
 }
+  }
+
 
 export default Saved_Recipes;
