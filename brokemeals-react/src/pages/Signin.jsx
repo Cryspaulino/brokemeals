37bdb320d951739b964337dcb8994010e1a0db22
Joyce Ili <131714669+JoyceJ53@users.noreply.github.com>
2025-06-23 17:35:15 -0600

    sign in css grid

--- brokemeals-react/src/pages/Signin.jsx
+++ brokemeals-react/src/pages/Signin.jsx
@@ -7,7 +7,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { auth, provider, database } from "../firebase/firebaseConfig";
 import React, { useState } from "react";
 import { ref, set } from "firebase/database";
-import "../styles/styles.css";
+import "../styles/styles-signin.css";
 import { useAuth } from "../context/AuthContext";
 
 function Signin() {
@@ -121,10 +121,12 @@ function Signin() {
       <button className="google-signin-button" onClick={signInWithGoogle}>
         Sign In with Google
       </button>
-      <p className ="signup-text">Don't have an account?</p>
-      <button className="signup-button" onClick={() => navigate("/signup")}>
+      <div className= "no-account-box">
+        <p className ="signin-signup-text">Don't have an account?</p>
+        <button className="signin-signup-button" onClick={() => navigate("/signup")}>
         Sign Up
-      </button>
+        </button>
+      </div>
     </div>
   );
 }
