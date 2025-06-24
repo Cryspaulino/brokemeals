4cfd18266adb5833c97c506a84f42aa63f2b6fff
Joyce Ili <131714669+JoyceJ53@users.noreply.github.com>
2025-06-23 18:16:06 -0600

    tutorial grid css

--- brokemeals-react/src/pages/Tutorial.jsx
+++ brokemeals-react/src/pages/Tutorial.jsx
@@ -7,28 +7,28 @@ function Tutorial() {
         <div className="tutorial-content-box">
             <h1 className ="tutorial-title">Tutorial</h1>
             <h3 className = "tutorial-subtitle">tutorial subtitle text here</h3>
-            <div className ="tutorial-steps-box">
+            <div className ="tutorial-steps-boxes">
                 <div className="tutorial-step-1-box">
                     <h2 className="step-1-title">Step 1</h2>
                     <h4 className="step-1-text">tutorial step 1 text here</h4>
-                    <img src="" alt="Step 1 Image" />
+                    <img src="" alt="step-1-image" />
                     <h4 className="step-1-text">tutorial step 1 text 2 here</h4>
                 </div>
                 <div className="tutorial-step-2-box">
                     <h2 className="step-2-title">Step 2</h2>
                     <h4 className="step-2-text">tutorial step 2 text here</h4>
-                    <img src="" alt="Step 2 Image" />
+                    <img src="" alt="step-2-image" />
                     <h4 className="step-2-text">tutorial step 2 text 2 here</h4>
                 </div>
                 <div className="tutorial-step-3-box">
                     <h2 className="step-3-title">Step 3</h2>
                     <h4 className="step-3-text">tutorial step 3 text here</h4>
-                    <img src="" alt="Step 3 Image" />
+                    <img src="" alt="step-3-image" />
                     <h4 className="step-3-text">tutorial step 3 text 2 here</h4>
                 </div>
             </div>
-            <Link to="/recipes" className="tutorial-get-started-button">
-                <button className="tutorial-get-started-text">Get Started</button>
+            <Link to="/recipes">
+                <button className="tutorial-get-started-button">Get Started</button>
             </Link>
         </div>
     );
